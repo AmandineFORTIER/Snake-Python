ddef7e72524294ca4912fb43619e93a6eba76bb6
Amandine FORTIER <amandinefor@gmail.com>
2020-11-03 18:31:56 +0100

    Start to clean the code

--- main.py
+++ main.py
@@ -15,13 +15,17 @@ WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 600
 
 
-def random_pos_on_grid():
+def random_pos_on_grid(step_size, width, height):
     """
-    :return: A random position on the map. It will be on a multiple of STEP_SIZE
+    Give a random position on the window and at a multiple of step_size
+    :param step_size: The step size
+    :param width: Window width
+    :param height: Windows height
+    :return: A random position on the map. It will be on a multiple of step_size
     """
     return [
-        randint(0, (int((WINDOW_WIDTH - STEP_SIZE) / STEP_SIZE))) * STEP_SIZE,
-        randint(0, (int((WINDOW_HEIGHT - STEP_SIZE) / STEP_SIZE))) * STEP_SIZE]
+        randint(0, (int((width - step_size) / step_size))) * step_size,
+        randint(0, (int((height - step_size) / step_size))) * step_size]
 
 
 class SnakeTail(Widget):
@@ -29,12 +33,12 @@ class SnakeTail(Widget):
     Tail of the snake
     """
 
-    def move(self, new_pos):
+    def move(self, pos):
         """
-        Move the snake tail
-        :param new_pos: New pos of the tail
+        Change the snake tail position
+        :param pos: New pos of the tail
         """
-        self.pos = new_pos
+        self.pos = pos
 
 
 class SnakeHead(Widget):
@@ -50,24 +54,6 @@ class SnakeHead(Widget):
         Move the snake head
         """
         self.pos = Vector(*self.velocity) + self.pos
-        self._on_touch_wall()
-
-    def _on_touch_wall(self):
-        """
-        Check if the snake head touch a wall
-        """
-        if (self.y < 0) or (self.y + STEP_SIZE > WINDOW_HEIGHT):
-            self.y = (int(WINDOW_HEIGHT / STEP_SIZE) * STEP_SIZE) - STEP_SIZE if (self.y < 0) else 0
-        if (self.x < 0) or (self.x + STEP_SIZE > WINDOW_WIDTH):
-            self.x = (int(WINDOW_WIDTH / STEP_SIZE) * STEP_SIZE) - STEP_SIZE if (self.x < 0) else 0
-
-    def is_touching(self, pos):
-        """
-        Check if snake head is touching a pos
-        :param pos: The pos we want to check is snake head is touching it
-        :return: True if snake head is touching it
-        """
-        return pos[0] <= self.pos[0] < pos[0] + STEP_SIZE and pos[1] <= self.pos[1] < pos[1] + STEP_SIZE
 
 
 class Fruit(Widget):
@@ -91,29 +77,80 @@ class SnakeGame(Widget):
     fruit = ObjectProperty(None)
     score = NumericProperty(0)
     tail = []
-    player_size = NumericProperty(STEP_SIZE)
     position_to_go = StringProperty("right")
     is_game_over = BooleanProperty(False)
 
-    def __init__(self, **kwargs):
+    def __init__(self, step_size, width, height, **kwargs):
+        """
+        Initialise the game
+        :param step_size: Size of the step of a snake
+        :param width: Window's width
+        :param height: Window's height
+        :param kwargs:
+        """
         super(SnakeGame, self).__init__(**kwargs)
-        Window.size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+        Window.size = (width, height)
+        self.step_size = step_size
+        self._init_keyboard()
+        self._set_objects_size()
+
+    def _set_objects_size(self):
+        """
+        Set the object size
+        """
+        self.snake_head.size = (self.step_size, self.step_size)
+        self.fruit.size = self.snake_head.size
+
+    def _init_keyboard(self):
+        """
+        Initialise the keyboard
+        """
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-        self.is_pressed = False
 
     def start(self):
         """
         Start the game.
-        :param vel: default velocity
         """
-        self.snake_head.pos = random_pos_on_grid()
-        self.snake_head.velocity = (STEP_SIZE, 0)
-        self._add_tail((self.snake_head.pos[0] - STEP_SIZE, self.snake_head.pos[1]))
-        self._add_tail((self.snake_head.pos[0] - (2 * STEP_SIZE), self.snake_head.pos[1]))
-        self.fruit.move(random_pos_on_grid())
+        self._add_objects_on_grid()
+        self._add_cells_to_tail()
 
-    def _add_tail(self, pos):
+    def _add_objects_on_grid(self):
+        """
+        Add snake head and fruit on the grid
+        """
+        self.snake_head.pos = random_pos_on_grid(self.step_size, Window.width, Window.height)
+        self.fruit.move(random_pos_on_grid(self.step_size, Window.width, Window.height))
+
+    def _add_cells_to_tail(self):
+        """
+        Add 2 cells to the tail.
+        :return:
+        """
+        self._growth_tail((self.snake_head.pos[0] - self.step_size, self.snake_head.pos[1]))
+        self._growth_tail((self.snake_head.pos[0] - (2 * self.step_size), self.snake_head.pos[1]))
+
+    def _snake_goes_to(self, direction):
+        """
+        Modify the snake direction
+        :param direction: right, left, up, down or stop to move the snake at this direction
+        """
+        if direction == "right":
+            self.snake_head.velocity = (self.step_size, 0)
+        elif direction == "left":
+            self.snake_head.velocity = (-self.step_size, 0)
+        elif direction == "up":
+            self.snake_head.velocity = (0, self.step_size)
+        elif direction == "down":
+            self.snake_head.velocity = (0, -self.step_size)
+        elif direction == "stop":
+            self.snake_head.velocity = (0, 0)
+
+    def _growth_tail(self, pos):
+        """
+        Growth the snake's tail
+        :param pos: The position where we want to add the new cell snake
+        """
         self.tail.append(
             SnakeTail(
                 pos=pos,
@@ -122,87 +159,90 @@ class SnakeGame(Widget):
         )
         self.add_widget(self.tail[-1])
 
+    def _on_touch_wall(self):
+        """
+        Check if the snake head touch a wall
+        """
+        if (self.snake_head.y < 0) or (self.snake_head.y + self.step_size > Window.height):
+            self.snake_head.y = (int(Window.height / self.step_size) * self.step_size) - self.step_size \
+                if (self.snake_head.y < 0) else 0
+        if (self.snake_head.x < 0) or (self.snake_head.x + self.step_size > Window.width):
+            self.snake_head.x = (int(Window.width / self.step_size) * self.step_size) - self.step_size \
+                if (self.snake_head.x < 0) else 0
+
+    def _snake_head_is_touching(self, obj):
+        """
+        Check if snake head is touching an object
+        :param obj: the object we want to check is snake head is touching it
+        :return: True if snake head is touching it
+        """
+        return obj.pos[0] <= self.snake_head.pos[0] < obj.pos[0] + self.step_size and \
+               obj.pos[1] <= self.snake_head.pos[1] < obj.pos[1] + self.step_size
+
     def update(self, dt):
         """
         Move the snake and check if it touch the fruit
         """
         if self.is_game_over:
-            self.snake_head.velocity = (0, 0)
+            self._snake_goes_to("stop")
             for c in self.tail:
                 c.velocity = (0, 0)
         else:
-            if self.position_to_go == 'up':
-                self.snake_head.velocity = (0, STEP_SIZE)
-            elif self.position_to_go == 'down':
-                self.snake_head.velocity = (0, -STEP_SIZE)
-            elif self.position_to_go == 'left':
-                self.snake_head.velocity = (-STEP_SIZE, 0)
-            elif self.position_to_go == 'right':
-                self.snake_head.velocity = (STEP_SIZE, 0)
-
+            self._on_touch_wall()
+            self._snake_goes_to(self.position_to_go)
             for i in range(1, len(self.tail)):
-                if self.snake_head.is_touching(self.tail[i].pos):
-                    print("TOUCH ", i)
+                if self._snake_head_is_touching(self.tail[i]):
                     self.is_game_over = True
-                    anim = Animation(opacity=0, duration=0.2)
-                    anim += Animation(opacity=1, duration=0.2)
-                    anim.repeat = True
-                    anim.start(self)
+                    self._flash_screen()
                     break
                 self.tail[-i].move(new_pos=self.tail[-(i + 1)].pos)
             if not self.is_game_over:
                 self.tail[0].move(new_pos=self.snake_head.pos)
                 self.snake_head.move()
-                self.on_touch_fruit()
+                if self._snake_head_is_touching(self.fruit):
+                    self._on_touch_fruit()
 
-    def on_touch_fruit(self):
+    def _flash_screen(self):
         """
-        The snake get the point if it touch the fruit. The fruit respawn and the snake tail growth.
+        Flash the window
         """
-        if self.snake_head.is_touching(self.fruit.pos):
-            self.score += 1
-            self.fruit.move(random_pos_on_grid())
-            self._add_tail(self.tail[-1].pos)
+        anim = Animation(opacity=0, duration=0.2)
+        anim += Animation(opacity=1, duration=0.2)
+        anim.repeat = True
+        anim.start(self)
 
-    def _going_down(self):
+    def _on_touch_fruit(self):
         """
-        Check if the snake is going down
+        The snake get the point. The fruit respawn and the snake tail growth.
         """
-        return self.snake_head.velocity_x == 0 and self.snake_head.velocity_y == -STEP_SIZE
+        self.score += 1
+        self.fruit.move(random_pos_on_grid(self.step_size, Window.width, Window.height))
+        self._growth_tail(self.tail[-1].pos)
 
-    def _going_up(self):
+    def _is_going_to(self, direction):
         """
-        Check if the snake is going up
+        check if the snake is going to a specific direction
+        :param direction: right, left, up or down
+        :return: true if the snake is going to this direction otherwise else
         """
-        return self.snake_head.velocity_x == 0 and self.snake_head.velocity_y == STEP_SIZE
-
-    def _going_left(self):
-        """
-        Check if the snake is going left
-        """
-        return self.snake_head.velocity_x == -STEP_SIZE and self.snake_head.velocity_y == 0
-
-    def _going_right(self):
-        """
-        Check if the snake is going right
-        """
-        return self.snake_head.velocity_x == STEP_SIZE and self.snake_head.velocity_y == 0
+        if direction == "right":
+            return self.snake_head.velocity_x == self.step_size and self.snake_head.velocity_y == 0
+        if direction == "left":
+            return self.snake_head.velocity_x == -self.step_size and self.snake_head.velocity_y == 0
+        if direction == "up":
+            return self.snake_head.velocity_x == 0 and self.snake_head.velocity_y == self.step_size
+        if direction == "down":
+            return self.snake_head.velocity_x == 0 and self.snake_head.velocity_y == -self.step_size
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         """
             Check if there is an arrow input
         """
-        if keycode[1] == 'up' and not self._going_down():
-            self.position_to_go = "up"
-            return True
-        elif keycode[1] == 'down' and not self._going_up():
-            self.position_to_go = "down"
-            return True
-        elif keycode[1] == 'left' and not self._going_right():
-            self.position_to_go = "left"
-            return True
-        elif keycode[1] == 'right' and not self._going_left():
-            self.position_to_go = "right"
+        if ((keycode[1] == 'up' and not self._is_going_to("down"))
+                or (keycode[1] == 'down' and not self._is_going_to("up"))
+                or (keycode[1] == 'left' and not self._is_going_to("right"))
+                or (keycode[1] == 'right' and not self._is_going_to("left"))):
+            self.position_to_go = keycode[1]
             return True
         return False
 
@@ -216,7 +256,7 @@ class SnakeGame(Widget):
 
 class SnakeApp(App):
     def build(self):
-        game = SnakeGame()
+        game = SnakeGame(STEP_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT)
         game.start()
         Clock.schedule_interval(game.update, 10.0 / 60.0)
         return game
